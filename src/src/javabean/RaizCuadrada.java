661a48e0cfe4720606f4da0e6441bff24d1bd9ac
Pablo Granados <pablogranadosrua@gmail.com>
2025-02-01 12:29:04 +0100

    Creación de las anotaciones JavaDoc

--- src/src/javabean/RaizCuadrada.java
+++ src/src/javabean/RaizCuadrada.java
@@ -1,6 +1,22 @@
 package javabean;
 
+/**
+ * La clase {@code RaizCuadrada} proporciona un metodo estático para calcular raíces cuadradas de un número que recibe como parametro.
+ *
+ * @author Pablo Granados
+ * @version 0.1
+ */
+
 public class RaizCuadrada {
+
+    /**
+     * Este metodo usa la función {@link Math#sqrt(double)} para realizar el cálculo y devuelve una {@link ArithmeticException} con el contenido "NaN" en caso de que el número
+     * recibido sea 0.
+     *
+     * @param numero Número del que se desea calcular la raíz cuadrada.
+     * @return La raíz cuadrada del número recibido.
+     * @throws ArithmeticException en caso de recibir 0.
+     */
 
     public static double calcularRaizCuadrada(double numero) {
 
