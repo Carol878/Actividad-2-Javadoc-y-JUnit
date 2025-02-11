43d0d5b4b1b192923910fe15cab22e3c5fa0c90d
Carol <nahir.torres329@comunidadunir.net>
2025-02-11 21:00:09 +0100

    Añado comentarios con las etiquetas correspondientes para generar el Javadoc.

--- src/Resta.java
+++ src/Resta.java
@@ -1,3 +1,9 @@
+/**
+ * Clase que realiza sumas dentro de una aplicación de calculadora.
+ * @author Nahir Carolina Torres
+ * @version Version 1.0
+ */
+
 public class Resta
 {
     // Variable para guardar acumulados
@@ -7,26 +13,48 @@ public class Resta
     public Resta (){
     }
 
-    // Creo función resta de 2 reales
+    /**
+     * Creo función resta de 2 reales.
+     * @param realA primer parámetro con el que se realizará la resta.
+     * @param realB segundo parámetro con el que se realizará la resta.
+     * @return resultado de la resta.
+     */
     double restaReales (double realA, double realB)
     {
         double resultado = realA - realB;
         return resultado;
     }
-    // Creo función resta de 2 enteros
+
+    /**
+     * Creo función resta de 2 enteros.
+     * @param enteroA primer parámetro con el que se realizará la resta.
+     * @param enteroB segundo parámetro con el que se realizará la resta.
+     * @return resultado de la resta.
+     */
     int restaEnteros (int enteroA, int enteroB)
     {
         int resultado = enteroA - enteroB;
         return resultado;
     }
-    // Creo función resta de 3 reales
+
+    /**
+     * Creo función resta de 3 reales.
+     * @param realA primer parámetro con el que se realizará la resta.
+     * @param realB segundo parámetro con el que se realizará la resta.
+     * @param realC tercer parámetro con el que se realizará la resta.
+     * @return resultado de la resta.
+     */
     double restaReales (double realA, double realB, double realC)
     {
         double resultado = realA - realB - realC;
         return resultado;
     }
 
-    // Resta con valor acumulados
+    /**
+     * Resta con valor acumulados, es decir, cada vez que se use se irá acumulando el valor en la variable resAcumulado.
+     * @param acumulado valor acumulado.
+     * @return resultado de la resta de los valores acumulados.
+     */
     double restaAcumulado (double acumulado)
     {
         resAcumulado = acumulado - resAcumulado;
