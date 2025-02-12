c2df17e0395b4494722c523414215cb783043cae
Juan Moreno Garoz <juangaroz98@gmail.com>
2025-02-12 12:49:51 +0100

    Update clase potencia

--- javabean/Potencia.java
+++ javabean/Potencia.java
@@ -1,14 +1,29 @@
 package javabean;
+import java.util.Scanner;
 
 public class Potencia {
     /** La clase Potencia crea un metodo para calcular la potencia de un número.
      * @author Juan Moreno */
-    public double calcularPotencia() {
+    public double calcularPotencia(int base, int exponente) {
         double resultado = 1;
         for (int i = 0; i < Math.abs(exponente); i++) {
             resultado *= base;
         }
         return (exponente < 0) ? 1 / resultado : resultado;
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Potencia potencia = new Potencia();
 
+        System.out.print("Escribe la base): ");
+        int base = scanner.nextInt();
+
+        System.out.print("Escribe el exponente: ");
+        int exponente = scanner.nextInt();
+
+        double resultado = potencia.calcularPotencia(base, exponente);
+        System.out.println("Resultado: " + resultado);
+
+        scanner.close();
+    }
 }
